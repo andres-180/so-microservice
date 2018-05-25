54a21530038e6b4ef1ed07aa2fc560d038fed952
andres-180 <belalcazar180@gmail.com>
2018-05-25 12:43:17 -0400

    Update

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -10,8 +10,20 @@ def test_get_cpu_percent(mocker,client):
   mocker.patch.object(Stats, 'get_cpu_percent',return_value=100)
   response=client.get('/v1/stats/cpu')
   assert response.data.decode('utf-8')=='{"cpu_percent": 100}'
-  assert response.status_code==200 
+  assert response.status_code==200
+ 
+def test_get_available_memory(mocker, client):
+  mocker.patch.object(Stats, 'get_available_memory', return_value=2000)
+  response = client.get('/v1/stats/memory')
+  assert response.data.decode('utf-8') == '{"memoria_disponible(MB)": 2000}'
+  assert response.status_code == 200
 
+
+def test_get_disk_space(mocker, client):
+  mocker.patch.object(Stats, 'get_disk_space', return_value=1000)
+  response = client.get('/v1/stats/disk')
+  assert response.data.decode('utf-8') == '{"espacio_en_disco(MB)": 1000}'
+  assert response.status_code == 200
 
 def test_dummy():
   assert 1==1
